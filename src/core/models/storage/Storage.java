8087caeef7df38ac283fa414bb0b359e96bb519b
mick-89 <mmick0689@gmail.com>
2024-11-19 19:54:59 -0500

    implementación completa de Storage

---
+++ src/core/models/storage/Storage.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package core.models.storage;
+
+public class Storage { // esto va a tener que ser como una interfaz o algo así por SOLID y que hay 3 modelos
+
+}
